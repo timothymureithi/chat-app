426ea8e6f7357ceef85e2301555d5e3a241a6bd3
Timothy Mureithi <timothy.mureithi@student.moringaschool.com>
2023-06-06 00:18:02 +0300

    add keys to firebaseConfig function

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,23 @@
-import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId: ,
-  };
-  
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  export const auth = getAuth(app);
-  export const db = getFirestore(app);
+  apiKey: "AIzaSyB_xW0FnYH-YGIf0hb5B032Og15ubJpj4g",
+  authDomain: "react-chat-37688.firebaseapp.com",
+  projectId: "react-chat-37688",
+  storageBucket: "react-chat-37688.appspot.com",
+  messagingSenderId: "632795087883",
+  appId: "1:632795087883:web:b559c2bf2e06e79d31330e",
+  measurementId: "G-FHFTV7PVX1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
