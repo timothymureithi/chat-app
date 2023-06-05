3c656b3b2ce10293d12b0984c7baacda42fbf9f0
Timothy Mureithi <timothy.mureithi@student.moringaschool.com>
2023-06-06 00:26:02 +0300

    implement function to authenticate users via gmail

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NavBar = () => {
+  const [user, setUser] = useState(false);
+  const googleSignIn = () => {
+    setUser(true);
+  };
   
+  const signOut = () => {
+    setUser(false);
+  };
+
+  return (
+    <nav className="nav-bar">
+    
+    </nav>
+  );
 };
 export default NavBar;
