61c2fd9f4e5f12af94c44f7f91525b769bdd4d00
Timothy Mureithi <timothy.mureithi@student.moringaschool.com>
2023-06-06 10:18:32 +0300

    import from firebase and replaced code for google signin/signout functions

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
+import { auth } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
 
 const NavBar = () => {
-  const [user, setUser] = useState(false);
-  const googleSignIn = () => {
-    setUser(true);
-  };
-  
-  const signOut = () => {
-    setUser(false);
-  };
+    const [user] = useAuthState(auth);
+    const googleSignIn = () => {
+        const provider = new GoogleAuthProvider();
+        signInWithRedirect(auth, provider);
+      };
+      const signOut = () => {
+        auth.signOut();
+      };
 
   return (
     <nav className="nav-bar">
