c47435169bd763755ab948b473ab4331e2dba963
Timothy Mureithi <timothy.mureithi@student.moringaschool.com>
2023-06-06 10:16:05 +0300

    set up google authentication

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,8 +12,22 @@ const NavBar = () => {
 
   return (
     <nav className="nav-bar">
-    
-    </nav>
+    <h1>React Chat</h1>
+    {user ? (
+      <button onClick={signOut} className="sign-out" type="button">
+        Sign Out
+      </button>
+    ) : (
+      <button className="sign-in">
+        <img
+          onClick={googleSignIn}
+        //   src={GoogleSignin}
+          alt="sign in with google"
+          type="button"
+        />
+      </button>
+    )}
+  </nav>
   );
 };
 export default NavBar;
