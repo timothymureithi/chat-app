da166c3ebe6edd9cca9dd473676920f062b1711d
Timothy Mureithi <timothy.mureithi@student.moringaschool.com>
2023-06-09 23:04:02 +0300

    import firebase authentication

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,5 +1,7 @@
 import React from "react";
 import GoogleSignin from "../img/btn_google_signin_dark_pressed_web.png";
+import { auth } from "../firebase";
+import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
 
 const Welcome = () => {
   const googleSignIn = () => {};
