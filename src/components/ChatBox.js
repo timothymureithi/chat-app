fdd3f23f6262ca78abe01106cb1df890b813f79e
Timothy Mureithi <timothy.mureithi@student.moringaschool.com>
2023-06-11 23:20:30 +0300

    add useEffect to query the db for messages

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -7,3 +7,30 @@ import {
   limit,
 } from "firebase/firestore";
 import { db } from "../firebase";
+
+const ChatBox = () => {
+    const [messages, setMessages] = useState([]);
+    
+    useEffect(() => {
+        const q = query(
+          collection(db, "messages"),
+          orderBy("createdAt"),
+          limit(50)
+        );
+        const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+          let messages = [];
+          QuerySnapshot.forEach((doc) => {
+            messages.push({ ...doc.data(), id: doc.id });
+          });
+          setMessages(messages);
+        });
+        return () => unsubscribe;
+      }, []);
+   
+    return (
+      <main className="chat-box">
+       
+      </main>
+    );
+  };
+  export default ChatBox;
