d18272d1525dbddad4926f52f3bc683ea87d4cee
Timothy Mureithi <timothy.mureithi@student.moringaschool.com>
2023-06-11 20:55:28 +0300

    import firebase dependancies to send messages

---
+++ src/components/SendMessage.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { auth, db } from "../firebase";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+
+const SendMessage = () => {
+  return (
+    <form className="send-message">
+      <label htmlFor="messageInput" hidden>
+        Enter Message
+      </label>
+      <input
+        id="messageInput"
+        name="messageInput"
+        type="text"
+        className="form-input__input"
+        placeholder="type message..."
+      />
+      <button type="submit">Send</button>
+    </form>
+  );
+};
+
+export default SendMessage;
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,7 +1,7 @@
 import React from "react";
 import GoogleSignin from "../img/btn_google_signin_dark_pressed_web.png";
 import { auth } from "../firebase";
-import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
+import { Google, signInWithRedirect } from "firebase/auth";
 
 const Welcome = () => {
   const googleSignIn = () => {
