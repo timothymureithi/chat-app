abc15ae98685961493ebb836eb30bbf29daf691f
Timothy Mureithi <timothy.mureithi@student.moringaschool.com>
2023-06-11 21:48:47 +0300

    add dependencies to retrieve messages from db

---
+++ src/components/ChatBox.js
@@ -0,0 +1,9 @@
+import { useEffect, useRef, useState } from "react";
+import {
+  query,
+  collection,
+  orderBy,
+  onSnapshot,
+  limit,
+} from "firebase/firestore";
+import { db } from "../firebase";
