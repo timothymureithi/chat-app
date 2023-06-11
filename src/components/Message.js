0a2cdf788e7c4a7722cd7ac92096eb5d541d954b
Timothy Mureithi <timothy.mureithi@student.moringaschool.com>
2023-06-11 23:35:17 +0300

    messages are received and they are updated in realtime

--- src/components/Message.js
+++ src/components/Message.js
@@ -4,10 +4,20 @@ import { useAuthState } from "react-firebase-hooks/auth";
 
 const Message = ({ message }) => {
   const [user] = useAuthState(auth);
+  
 
   return (
-   <div>
-    
+    <div
+    className={`chat-bubble ${message.uid === user.uid ? "right" : ""}`}>
+    <img
+      className="chat-bubble__left"
+      src={message.avatar}
+      alt="user avatar"
+    />
+    <div className="chat-bubble__right">
+      <p className="user-name">{message.name}</p>
+      <p className="user-message">{message.text}</p>
+    </div>
    </div>
   );
 };
